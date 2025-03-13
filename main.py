1a8b7d889876ed443c7fd40e02c70d692bf7cfb7
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-03-13 02:53:11 -0500

    Smth

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import get_word_count
+
+def get_book_text(filename):
+    with open(filename) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    test_var = "./books/frankenstein.txt"
+    file_contents = get_book_text(test_var)
+    num_words = get_word_count(file_contents)
+    print(f"{num_words} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(content):
+    # file_contents = get_book_text(filename)
+    return len(content.split())
