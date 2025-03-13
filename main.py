fb7dff0dea274ed4c06f24d331733188564cd8bd
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-03-13 15:25:27 -0500

    completed bookbot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_word_count
+from stats import get_word_count, get_num_letter_count, sorted_num_letter_count
+import sys 
 
 def get_book_text(filename):
     with open(filename) as f:
@@ -7,9 +8,24 @@ def get_book_text(filename):
 
 
 def main():
-    test_var = "./books/frankenstein.txt"
-    file_contents = get_book_text(test_var)
+    # test_var = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}")
+    file_contents = get_book_text(path_to_book)
+    
+    print("----------- Word Count ----------")
     num_words = get_word_count(file_contents)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    letter_count_list = sorted_num_letter_count(get_num_letter_count(file_contents))
+    for item in letter_count_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def get_word_count(content):
-    # file_contents = get_book_text(filename)
     return len(content.split())
+
+def get_num_letter_count(content):
+    letter_dict = {}
+    for i in content.lower():
+        if i not in letter_dict:
+            letter_dict[i] = content.lower().count(i)
+    return letter_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def sorted_num_letter_count(letter_dict):
+    foo = []
+    for item in letter_dict:
+        foo.append({"char": item, "count": letter_dict[item]})
+    foo.sort(key=sort_on, reverse=True)
+    return foo
